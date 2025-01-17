0502c29ae8abac6d064b83136b54132c69da9c69
HARSH MANIAR <harshmaniarofficial@gmail.com>
2025-01-17 23:23:57 +0530

    added the UI to header and hero components and added the basic dashboard page on routes

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="bg-white dark:bg-gray-900">
+    <header className="bg-black">
       <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-        {/* <Image src={}/> */}
+        <Image src="/logo-black.png" alt="Logo" width={100} height={100} />
 
         <div className="flex flex-1 items-center justify-end md:justify-between">
           <nav aria-label="Global" className="hidden md:block">
             <ul className="flex items-center gap-6 text-sm">
               <li>
                 <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-500 transition hover:text-white dark:text-white dark:hover:text-white"
                   href="#"
                 >
                   About
@@ -21,7 +21,7 @@ function Header() {
 
               <li>
                 <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-500 transition hover:text-white dark:text-white dark:hover:text-white"
                   href="#"
                 >
                   Careers
@@ -30,7 +30,7 @@ function Header() {
 
               <li>
                 <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-500 transition hover:text-white dark:text-white dark:hover:text-white"
                   href="#"
                 >
                   History
@@ -39,7 +39,7 @@ function Header() {
 
               <li>
                 <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-500 transition hover:text-white dark:text-white dark:hover:text-white"
                   href="#"
                 >
                   Services
@@ -48,35 +48,28 @@ function Header() {
 
               <li>
                 <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                  className="text-gray-500 transition hover:text-white dark:text-white dark:hover:text-white"
                   href="#"
                 >
                   Projects
                 </a>
               </li>
 
-              <li>
-                <a
-                  className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
-                  href="#"
-                >
-                  Blog
-                </a>
-              </li>
+              
             </ul>
           </nav>
 
           <div className="flex items-center gap-4">
             <div className="sm:flex sm:gap-4">
               <a
-                className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700 dark:hover:bg-teal-500"
+                className="block rounded-md  px-5 py-2.5 text-sm font-medium text-white transition  dark:hover:bg-teal-500"
                 href="#"
               >
                 Login
               </a>
 
               <a
-                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 transition hover:text-teal-600/75 sm:block dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
+                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium  transition hover:text-slate-900 sm:block dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
                 href="#"
               >
                 Register
