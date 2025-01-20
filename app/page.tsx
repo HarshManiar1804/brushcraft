43d9f662833c0312874a1bdea3c5d98ffc2ef7e0
HARSH MANIAR <harshmaniarofficial@gmail.com>
2025-01-20 23:34:58 +0530

    log the user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import Image from "next/image";
 import { Button } from "../components/ui/button";
 import Hero from "./_components/Hero";
 import Header from "./_components/Header";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useEffect } from "react";
 
 export default function Home() {
+  // loged in user details
+  const { user } = useKindeBrowserClient();
+  console.log(user);
+
   return (
     <div>
       <Header />
