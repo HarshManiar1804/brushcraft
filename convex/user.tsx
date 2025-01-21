8670ff12f192e57a6a7fc0807a08fc0904266f2c
HARSH MANIAR <harshmaniarofficial@gmail.com>
2025-01-21 13:01:43 +0530

    added the create user and get user function to fetch the data from the database

---
+++ convex/user.tsx
@@ -0,0 +1,22 @@
+import { v } from 'convex/values';
+import { mutation, query } from './_generated/server';
+export const getUser = query({
+    args: {
+        email: v.string()
+    },
+    handler: async (ctx, args) => {
+        const result = await ctx.db.query('user').filter((q) => q.eq(q.field('email'), args.email)).collect();
+        return result;
+    }
+})
+export const createUser = mutation({
+    args: {
+        name: v.string(),
+        email: v.string(),
+        image: v.string(),
+    },
+    handler: async (ctx, args) => {
+        const result = await ctx.db.insert('user', { email: args.email, name: args.name, simage: args.image });
+        return result;
+    }
+});
