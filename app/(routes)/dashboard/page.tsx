824d2180b7433a503555c1e1578bfde426992315
HARSH MANIAR <harshmaniarofficial@gmail.com>
2025-01-21 12:19:31 +0530

    added the logout feature and retrict the dashboard page for unauthorized user

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return <div>Dashboard
+    <LogoutLink>
+      <Button>Logout</Button>
+    </LogoutLink>
+  </div>;
 }
 
 export default Dashboard;
