142bbfa3d96cf69e59110e28bca9975a71ed39f8
HARSH MANIAR <harshmaniarofficial@gmail.com>
2025-01-21 18:09:37 +0530

    added the create team mutation to store the team name in database

--- app/(routes)/teams/create/page.tsx
+++ app/(routes)/teams/create/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
+import Image from 'next/image';
+import React, { useRef, useState } from 'react'
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button';
+import { useMutation } from 'convex/react';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
+import { toast } from 'sonner';
+import { api } from '@/convex/_generated/api';
+import { useRouter } from 'next/navigation';
 
 function CreateTeam() {
+    const router = useRouter();
+    const { user }: any = useKindeBrowserClient();
+    const [teamName, setTeamName] = useState<string>('');
+    const createTeam = useMutation(api.teams.createTeam);
+
+    const createNewTeam = () => {
+        createTeam({
+            teamName: teamName,
+            createdBy: user?.email
+        }).then((resp) => {
+            console.log(resp);
+            if (resp) {
+                toast.success('Team created successfully!!!');
+                router.push('/dashboard');
+            }
+        })
+    }
+    const handleCreateTeam = () => {
+        createNewTeam();
+    }
     return (
-        <div>
-            CreateTeam
+        <div className='px-16 my-16 md:px-16'>
+            <Image src='/logo-black.png' alt='logo' width={200} height={200} />
+
+            <div className="flex flex-col items-center">
+                <h2 className='font-bold text-[40px] py-3'>What should we call your team?</h2>
+                <h2 className='text-gray-500'>You can always change this later from settings</h2>
+                <div className="mt-7 w-[40%]">
+                    <label className='text-gray-500'>Team Name</label>
+                    <Input type="text" placeholder="Team name" className='mt-3'
+                        onChange={(e) => setTeamName(e.target.value)} />
+                </div>
+                <Button onClick={handleCreateTeam} className='bg-blue-500 mt-9 w-[30%] hover:bg-blue-700'>Create team</Button>
+            </div>
         </div>
     )
 }
--- convex/teams.tsx
+++ convex/teams.tsx
@@ -8,4 +8,12 @@ export const getTeam = query({
         const result = await ctx.db.query('teams').filter((q) => q.eq(q.field('createdBy'), args.email)).collect();
         return result;
     }
-}) 
+})
+
+export const createTeam = mutation({
+    args: { teamName: v.string(), createdBy: v.string() },
+    handler: async (ctx, args) => {
+        const result = await ctx.db.insert('teams', args);
+        return result;
+    },
+})
